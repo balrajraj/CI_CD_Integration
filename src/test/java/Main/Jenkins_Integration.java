bdc3565d286325efc7376309ab7e5632656d7b0c
balrajraj <balrajullagaddi@yahoo.com>
2020-08-19 12:00:33 +0530

    commit 2

--- src/test/java/Main/Jenkins_Integration.java
+++ src/test/java/Main/Jenkins_Integration.java
@@ -1,9 +1,23 @@
 package Main;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Jenkins_Integration {
+	
+	static WebDriver driver;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver().setup();
+		
+		driver = new ChromeDriver();
+		
+		driver.get("https://www.google.co.in/");
 		
 		System.out.println("Hi Balraj");
 
