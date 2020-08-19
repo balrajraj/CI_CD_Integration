18d46513369ef7033c0d842af7c358b32b94e67f
balrajraj <balrajullagaddi@yahoo.com>
2020-08-19 13:40:07 +0530

    commit 10

--- src/test/java/Main/Jenkins_Integration.java
+++ src/test/java/Main/Jenkins_Integration.java
@@ -4,8 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-import io.github.bonigarcia.wdm.ChromeDriverManager;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Jenkins_Integration {
 	
